9bcf79f8dfe8921a8f1c2c197e0a9456bddd1ae7
mix <mixfoodev>
2022-01-29 00:32:44 +0200

    readme added

---
+++ README.md
@@ -0,0 +1,10 @@
+# README
+
+This is a simple Python script that can rename multiple filenames and replace multiple words in files.
+
+**How it works**
+
+- Set the parent directory of the files you want to rename
+- Provide a file that contains the words to be replaced in key/value pairs. Eg. word:replacement
+
+A sample is provided in order to help you better understand how it works.
--- renamer.py
+++ renamer.py
@@ -196,14 +196,14 @@ WORDS_FILE = "words.txt"
 KEY_VALUE_SEPERATOR= ":"
 
 # If a folder name, inside parent directory, matches an entry then this folder will be ignored
-IGNORE_FOLDERS = ["inside parent folder"]
+IGNORE_FOLDERS = ["some_folder"]
 
 # File types we want to edit
 FILE_TYPES = ["txt"]
 
 # If a replaceable word has a char from this list before or after it, it will be replaced, else will be ignored.
 #CHARS_NEIGHBOURS_ALLOWED = list(" ,./*-+()[]{\}?@!=<>&$#%|;:\"")
-CHARS_NEIGHBOURS_ALLOWED = list(" ,.-")
+CHARS_NEIGHBOURS_ALLOWED = list(" ,.")
 
 # Starts the script
 run()
